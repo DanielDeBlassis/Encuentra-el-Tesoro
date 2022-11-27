eb71f824e6cfb639895908141c9f5fb3fc5ff711
Daniel <98892382+DanielDeBlassis@users.noreply.github.com>
2022-11-27 20:51:03 -0300

    Crea función escucha evento click
    La misma toma el evento, incrementa en uno el número de clicks, guarda la distancia, recurriendo a una función creada previamente, y luego muestra un mensaje indicando qué tan cerca se está del tesoro.
    Finalmente se establece una condición en la que si la distancia del click con el tesoro es menor a 10, el jugador encontró el tesoro y el juego terminó. Se emite una alerta indicando de que ganó, y al hacer click para cerrarla la página se recarga, reiniciando el juego.

--- js/game.js
+++ js/game.js
@@ -8,3 +8,30 @@ let target = {
 
 const $mapa = document.querySelector("#mapa");
 let nroClicks = 0;
+
+$mapa.addEventListener("click", (e) => {
+    nroClicks++;
+    let distance = getDistance(e, target);
+    
+    getDistanceHint(distance);
+
+    if (distance < 10) {
+
+        const contenedorImagen = document.createElement("div");
+        contenedorImagen.classList.add("flex", "w-full", "justify-center", "content-center", "items-center", "text-center");
+        const imagen = document.createElement("img");
+        imagen.src = "img/treasure.svg";
+        imagen.classList.add("w-[100px]");
+        contenedorImagen.appendChild(imagen);
+
+        swal({
+            title: "Felicidades! Ha encontrado el tesoro!",
+            text: `Lo has encontrado luego de ${nroClicks} clicks.`,
+            content: contenedorImagen,
+            dangerMode: true,
+            className: "bg-[#cccccc]",
+        })
+            .then(() => {
+                window.location.reload();
+            });
+    }
